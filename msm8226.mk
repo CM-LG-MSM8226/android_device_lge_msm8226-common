9e8a7150951f174542886a0dc4abbece79f747a9
Dan Levinson <danholandi@gmail.com>
2017-09-18 21:34:58 +0300

    msm8226-common: remove unsed hostapd config files
    Change-Id: Id2201e52a0a8706291cfd1900a147dfeb5cceb9b

--- msm8226.mk
+++ msm8226.mk
@@ -213,10 +213,7 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf \
     wpa_supplicant_overlay.conf \
-    p2p_supplicant_overlay.conf \
-    hostapd_default.conf \
-    hostapd.accept \
-    hostapd.deny
+    p2p_supplicant_overlay.conf
 
 # Telephony-ext
 PRODUCT_PACKAGES += telephony-ext
