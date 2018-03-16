72033694941cbfdcadb7eca8420d83ecfc898ba3
vm03 <vasy@vasy.ru>
2018-03-16 14:23:58 +0300

    msm8226-common: return passthrough sensors hal
    Change-Id: I43e32b4465e65aafb69a54e582f8edb323eeac45

--- msm8226.mk
+++ msm8226.mk
@@ -216,7 +216,6 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service \
     sensors.msm8226 \
     sensors.qcom
 
